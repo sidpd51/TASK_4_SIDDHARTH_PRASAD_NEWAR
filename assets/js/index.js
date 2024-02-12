723e086f49254e83802d5ccb78910952af444999
siddharthpd <sidpd510@gmail.com>
2024-02-12 19:36:46 +0530

    grid-section-complete

---
+++ assets/js/index.js
@@ -0,0 +1,8 @@
+function productOne(){
+    document.getElementById('product-heading').innerHTML="What is this Product?";
+    document.getElementById('product-paragraph').innerHTML="This is product one paragraph. There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing.";
+}
+function productTwo(){
+    document.getElementById('product-heading').innerHTML="Why to buy this Product?";
+    document.getElementById('product-paragraph').innerHTML="This is product two paragraph. There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing.";
+}
